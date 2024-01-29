3ba7b3b13c5e3a9cd78d6f05475915efaa4e2025
andrei.rad <rad29ndrei@gmail.com>
2024-01-29 21:19:27 +0200

    PP-20: Dashboard Structure

--- ping-patrol-web-client/src/App.tsx
+++ ping-patrol-web-client/src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import HomeWrapperPage from "./pages/HomeWrapperPage";
 import MonitoringPage from "./pages/MonitoringPage";
 import IntegrationPage from "./pages/IntegrationPage";
+import DashboardPage from "./pages/Dashboard";
 
 const App: React.FC = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomeWrapperPage />}>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<MonitoringPage />} /> {/* Add index attribute */}
         <Route path="/monitoring" element={<MonitoringPage />} />
         <Route path="/integration" element={<IntegrationPage />} />
       </Route>
-
-      {/* other routes */}
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="*" element={<div>404</div>} />
     </Routes>
   </Router>
 );
--- ping-patrol-web-client/src/components/TopMenuBar.tsx
+++ ping-patrol-web-client/src/components/TopMenuBar.tsx
@@ -16,12 +16,25 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
+import { useKeycloak } from "@react-keycloak/web";
 
 const StyledToolbar = styled(Toolbar)(() => ({
   justifyContent: "space-between",
 }));
 
 const TopMenuBar: React.FC = () => {
+  const { keycloak, initialized } = useKeycloak();
+
+  const handleLogin = () => {
+    if (!keycloak.authenticated) {
+      keycloak.login();
+    }
+  };
+
+  if (!initialized) {
+    return <div>Loading...</div>;
+  }
+
   const navigate = useNavigate();
   const [drawerOpen, setDrawerOpen] = useState(false);
   const theme = useTheme();
@@ -63,9 +76,15 @@ const TopMenuBar: React.FC = () => {
           ))}
         </List>
       </Drawer>
-      <Button color="inherit" onClick={() => navigate("/login")}>
-        Login
-      </Button>
+      {keycloak.authenticated ? (
+        <Button color="inherit" onClick={() => navigate("/dashboard")}>
+          Dashboard
+        </Button>
+      ) : (
+        <Button color="inherit" onClick={() => handleLogin()}>
+          Login
+        </Button>
+      )}
     </>
   );
 
@@ -83,19 +102,30 @@ const TopMenuBar: React.FC = () => {
         ))}
       </Box>
       <Box sx={{ marginRight: "10%" }}>
-        <Button color="inherit" onClick={() => navigate("/login")}>
-          Login
-        </Button>
+        {keycloak.authenticated ? (
+          <Button color="inherit" onClick={() => navigate("/dashboard")}>
+            Dashboard
+          </Button>
+        ) : (
+          <Button color="inherit" onClick={() => handleLogin()}>
+            Login
+          </Button>
+        )}
       </Box>
     </>
   );
 
   return (
-    <AppBar position="sticky" style={{ backgroundColor: '#131A25' }}>
+    <AppBar position="sticky" style={{ backgroundColor: "#131A25" }}>
       <StyledToolbar>
         <div style={{ justifyContent: "space-between" }}>
           <Box display="flex" alignItems="center" sx={{ marginLeft: "80%" }}>
-            <IconButton edge="start" color="inherit" aria-label="logo" onClick={() => navigate("/monitoring")}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="logo"
+              onClick={() => navigate("/monitoring")}
+            >
               <img
                 src="src/assets/ping-patrol-logo-color.png"
                 alt="Logo"
