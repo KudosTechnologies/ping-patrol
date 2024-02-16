ec83328be8ad89e867c5a7adfa52705bfc25d745
andrei.rad <rad29ndrei@gmail.com>
2024-02-16 17:37:09 +0200

    PP-43: Acceptance Tests and Reports

--- ping-patrol-rest-service/src/acceptance/java/ro/kudostech/pingpatrol/configuration/TestContext.java
+++ ping-patrol-rest-service/src/acceptance/java/ro/kudostech/pingpatrol/configuration/TestContext.java
@@ -4,7 +4,9 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ro.kudostech.pingpatrol.api.ApiClient;
+import ro.kudostech.pingpatrol.api.client.MonitorsApi;
 import ro.kudostech.pingpatrol.step.AuthorizationInterceptor;
+import ro.kudostech.pingpatrol.utils.TokenUtils;
 
 @Component
 public class TestContext {
@@ -22,5 +24,9 @@ public class TestContext {
     ApiClient apiClient = new ApiClient(restTemplate);
     apiClient.setBasePath(testRestTemplate.getRootUri());
     return apiClient;
+  }
+
+  public final MonitorsApi getMonitorsApiWithAdminAccess() {
+    return new MonitorsApi(initClient(TokenUtils.generateAdminJWT()));
   }
 }
